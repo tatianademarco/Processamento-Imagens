8b0fc64416f1c400eae19d90a115d269222453ec
Tatiana Maria Demarco <tatimariademarco@gamil.com>
2024-03-28 19:35:53 -0300

    commit inicial

---
+++ ProcessamentodeImagem/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ProcessamentodeImagemAula18._03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnNegativo = new System.Windows.Forms.Button();
+            this.btnBrilho = new System.Windows.Forms.Button();
+            this.btnContraste = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Caregar imagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnNegativo
+            // 
+            this.btnNegativo.Location = new System.Drawing.Point(163, 30);
+            this.btnNegativo.Name = "btnNegativo";
+            this.btnNegativo.Size = new System.Drawing.Size(125, 44);
+            this.btnNegativo.TabIndex = 1;
+            this.btnNegativo.Text = "Negativo";
+            this.btnNegativo.UseVisualStyleBackColor = true;
+            this.btnNegativo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnBrilho
+            // 
+            this.btnBrilho.Location = new System.Drawing.Point(330, 30);
+            this.btnBrilho.Name = "btnBrilho";
+            this.btnBrilho.Size = new System.Drawing.Size(125, 44);
+            this.btnBrilho.TabIndex = 2;
+            this.btnBrilho.Text = "Brilho";
+            this.btnBrilho.UseVisualStyleBackColor = true;
+            this.btnBrilho.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnContraste
+            // 
+            this.btnContraste.Location = new System.Drawing.Point(499, 30);
+            this.btnContraste.Name = "btnContraste";
+            this.btnContraste.Size = new System.Drawing.Size(125, 44);
+            this.btnContraste.TabIndex = 3;
+            this.btnContraste.Text = "Contraste";
+            this.btnContraste.UseVisualStyleBackColor = true;
+            this.btnContraste.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 196);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(20, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(350, 196);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnContraste);
+            this.groupBox1.Controls.Add(this.btnNegativo);
+            this.groupBox1.Controls.Add(this.btnBrilho);
+            this.groupBox1.Location = new System.Drawing.Point(12, 348);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(787, 90);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opções";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(388, 252);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Original";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox2);
+            this.groupBox3.Location = new System.Drawing.Point(406, 67);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(388, 252);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Editada";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnNegativo;
+        private System.Windows.Forms.Button btnBrilho;
+        private System.Windows.Forms.Button btnContraste;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ ProcessamentodeImagem/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessamentodeImagemAula18._03
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap originalBitmap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap contrasteBitmap = AumentarContraste(originalBitmap);
+
+            pictureBox2.Image = contrasteBitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Todos os arquivos|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string imagePath = openFileDialog.FileName;
+
+                originalBitmap = new Bitmap(imagePath);
+
+                pictureBox1.Image = originalBitmap;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap negativeBitmap = TransformarNegativo(originalBitmap);
+
+            pictureBox2.Image = negativeBitmap;
+        }
+
+        private Bitmap TransformarNegativo(Bitmap originalBitmap)
+        {
+            int width = originalBitmap.Width;
+            int height = originalBitmap.Height;
+
+            Bitmap negativeBitmap = new Bitmap(width, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color originalColor = originalBitmap.GetPixel(x, y);
+
+                    int invertedR = 255 - originalColor.R;
+                    int invertedG = 255 - originalColor.G;
+                    int invertedB = 255 - originalColor.B;
+
+                    Color negativeColor = Color.FromArgb(invertedR, invertedG, invertedB);
+
+                    negativeBitmap.SetPixel(x, y, negativeColor);
+                }
+            }
+
+            return negativeBitmap;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap brilhoBitmap = AumentarBrilho(originalBitmap);
+
+            pictureBox2.Image = brilhoBitmap;
+        }
+
+        private Bitmap AumentarBrilho(Bitmap originalBitmap)
+        {
+            int width = originalBitmap.Width;
+            int height = originalBitmap.Height;
+
+            Bitmap brilhoBitmap = new Bitmap(width, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color originalColor = originalBitmap.GetPixel(x, y);
+
+                    int brilhoR = originalColor.R + 50;
+                    if (brilhoR > 255)
+                    {
+                        brilhoR = 255;
+                    }
+
+                    int brilhoG = originalColor.G + 50;
+                    if (brilhoG > 255)
+                    {
+                        brilhoG = 255;
+                    }
+
+                    int brilhoB = originalColor.B + 50;
+                    if (brilhoB > 255)
+                    {
+                        brilhoB = 255;
+                    }
+
+
+                    Color brilhoColor = Color.FromArgb(brilhoR, brilhoG, brilhoB);
+
+                    brilhoBitmap.SetPixel(x, y, brilhoColor);
+                }
+            }
+
+            return brilhoBitmap;
+        }
+
+        private Bitmap AumentarContraste(Bitmap originalBitmap)
+        {
+            int width = originalBitmap.Width;
+            int height = originalBitmap.Height;
+
+            Bitmap brilhoBitmap = new Bitmap(width, height);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color originalColor = originalBitmap.GetPixel(x, y);
+
+                    int brilhoR = originalColor.R - 50;
+                    if (brilhoR < 0)
+                    {
+                        brilhoR = 0;
+                    }
+
+                    int brilhoG = originalColor.G - 50;
+                    if (brilhoG < 0)
+                    {
+                        brilhoG = 0;
+                    }
+
+                    int brilhoB = originalColor.B - 50;
+                    if (brilhoB < 0)
+                    {
+                        brilhoB = 0;
+                    }
+
+
+                    Color brilhoColor = Color.FromArgb(brilhoR, brilhoG, brilhoB);
+
+                    brilhoBitmap.SetPixel(x, y, brilhoColor);
+                }
+            }
+
+            return brilhoBitmap;
+        }
+    }
+}
